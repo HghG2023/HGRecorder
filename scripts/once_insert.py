0f19e9bcda73311145ca6a79f6afe9c95c77913b
HghG2023 <1264661272@qq.com>
2025-10-25 17:54:06 +0800

    detail change in once.py

--- scripts/once_insert.py
+++ scripts/once_insert.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-db_path = "./userdata/events_data_1021.db"  # 或你的 PM.get_env("EVENTS_DBNEW_PATH")
+db_path = "../userdata/events_data_1021.db"  # 或你的 PM.get_env("EVENTS_DBNEW_PATH")
 conn = sqlite3.connect(db_path)
 cur = conn.cursor()
 
@@ -13,7 +13,7 @@ to_add = {"ner_extract":None,
         "schema_version":1}
 
 
-for key, value in to_add:
+for key, value in to_add.items():
     if key not in columns:
         cur.execute(f"ALTER TABLE events ADD COLUMN {key} INTEGER DEFAULT {value};")
         conn.commit()
