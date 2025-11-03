ab0c86ab86e103cd8b6e567ee036b16102aa4ce0
HghG2023 <1264661272@qq.com>
2025-11-03 11:30:21 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,2 @@
 
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate HGRecorder
-
-# 运行项目
-python run.py
-
-# 程序结束后保持窗口打开（仅在手动运行时）
-read -p "程序已结束，按回车键退出..."
+nohup bash -c "source ~/miniconda3/bin/activate HGRecorder && python /home/admin/hgrecorder/run.py" > app.log 2>&1 &
